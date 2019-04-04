34878bf2ac970cb603a662b1bfa347a03c873db1
Julia Magyar <magyar.juliar@gmail.com>
2019-04-04 18:28:59 -0400

    setting up mern build

---
+++ routes/api/schedules.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+//const booksController = require("../../controllers/booksController");
+
+// Matches with "/api/books"
+router.route("/schedules")
+//   .get(booksController.findAll)
+//   .post(booksController.create); //until the controller is ALIVE
+
+// Matches with "/api/books/:id"
+router
+  .route("/:id")
+//   .get(booksController.findById)
+//   .put(booksController.update)
+//   .delete(booksController.remove);
+
+module.exports = router;
--- src/models/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    Expense: require("./Expenses"),
-    Household: require("./Household"),
-    Finance: require("./Finance"),
-    Schedule: require("./Schedule"),
-    User: require("./User")
-};
