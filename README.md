c9ad6927ba35a50ec33d11bbbdfa522d5487189d
Mtnman <billman.designs@gmail.com>
2019-04-04 18:31:56 -0400

    created some page files / npm react responsive (media query) / readme update

--- README.md
+++ README.md
@@ -7,8 +7,9 @@ The app will tie into your Google account which will set calendar reminders to a
 Set goals for your family and remember to treat yourself when a goal is met.
 
 ## New Techs Planned to be used
-- Sass
+- Google Sign In
 - Google Calendar API
+- React-Responsive
 
 ## Database Type
 - Mongo / Mongoose
--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./pages/Login/index";
 import Dashboard from "./pages/Dashboard/index";
+import Expenses from "./pages/Expenses/index";
+import Finances from "./pages/Finances/index";
 import Nav from "./components/NavBar/index"
 
 class App extends Component {
@@ -14,6 +16,8 @@ class App extends Component {
             <Switch>
               <Route exact path="/" component={Login} />
               <Route exact path="/dashboard" component={Dashboard} />
+              <Route exact path="/expenses" component={Expenses} />
+              <Route exact path="/finances" component={Finances} />
             </Switch>
           </div>
         </Router>
