d1f987fa6c9c33317a48356c582ca3a7327172de
Julia Magyar <magyar.juliar@gmail.com>
2019-04-10 20:24:23 -0400

    edit event model

--- models/Schedule.js
+++ models/Schedule.js
@@ -2,27 +2,40 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ScheduleSchema = new Schema({
-    event: {
+    title: {
         type: String,
         required: true
     },
-    description: {
+    start:{ //yyyy-mm-ddThh:mm:ss 24hr clock
         type: String,
-        required: false
-    },
-    category: {
-        type: String,
-        required: false
-    },
-    date: {
-        type: Date,
         required: true
     },
-    recurring: {
+    end: { //if omitted event will have default end (1 day for all day otherwise 1 hour) DATE IS EXCLUSIVE
+        type: String,
+        required: false
+    },
+    allDay: { 
         type: Boolean,
-        required: true
+        required: false
     },
-    frequency: {
+    //the following are for recurring events. If any of the following is entered the event is assumed to be recurring, defaulting to daily
+    daysOfWeek:[{//weekdays are enum starting at Sunday = 0, can have multiple days. Omit for daily
+        type: Number,
+        required: false
+    }],
+    startTime:{
+        type: String,
+        required: false
+    },
+    endTime:{
+        type: String,
+        required: false
+    },
+    startRecur:{
+        type: String,
+        required: false
+    },
+    endRecur:{
         type: String,
         required: false
     }
