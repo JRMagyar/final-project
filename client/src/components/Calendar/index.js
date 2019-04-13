6326f668c3202ccf9e27f940de05e5653ad8c550
Julia Magyar <magyar.juliar@gmail.com>
2019-04-13 09:39:13 -0400

    Schedule displaying on Calendar

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -7,23 +7,47 @@ import "./main.css"
 
 export default class Calendar extends React.Component {
   state = {
-    events : {
-      title: "Default Event",
-      start: "2019-04-2",
-      allDay: true
-    }
-  }
+    testEvents : [
+      {allDay: true,
+      start: "2019-04-12",
+      title: "Test Event 01"
+    },
+    {allDay: false,
+    start: "2019-04-14T13:30:00",
+    title: "Test Event 02"}
+  ]
+}
   componentDidMount(){
-    this.loadSchedule()
+    this.buildSchedule();
     // console.log(this.state.events)
   }
   
-  loadSchedule(){
+  // loadSchedule(){
+  //   this.getSchedule("5cafbbab08fb442350951b1f")
+  //   .then(res => this.setState({events: res.data},function(){
+  //     console.log(this.state.events)
+  //   }))
+  //   .catch(err => console.log(err))
+  // }
+  buildSchedule(){
     this.getSchedule("5cafbbab08fb442350951b1f")
-    .then(res => this.setState({events: res.data},function(){
-      console.log(this.state.events)
-    }))
-    .catch(err => console.log(err))
+    .then(res =>
+      {
+        console.log(res.data)
+        var schedule =[]
+        
+        for(let i = 0; i < res.data.length; i++){
+          let event = {}
+          event.title = res.data[i].title
+          event.start = res.data[i].start
+          event.allDay = res.data[i].allDay
+          schedule.push(event)
+        }
+        this.setState({events: schedule})
+        console.log(this.state.events)
+      }
+    )
+
   }
   getSchedule = id => {
     return axios.get("/allSchedule/" + id);
@@ -35,7 +59,7 @@ export default class Calendar extends React.Component {
       <FullCalendar
         showNonCurrentDates={false} 
         // height={530} 
-        events={[this.state.events]}
+        events={this.state.testEvents}
         
         fixedWeekCount={false}
         defaultView="dayGridMonth" 
