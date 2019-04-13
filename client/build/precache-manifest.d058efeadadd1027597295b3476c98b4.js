65ed743c5f7531b12a5b0478fee4400e0f5ba0a4
Julia Magyar <magyar.juliar@gmail.com>
2019-04-13 13:18:41 -0400

    added seed file

---
+++ client/build/precache-manifest.d058efeadadd1027597295b3476c98b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3917d0a72356c9813c5bf34bc08ce52e",
+    "url": "/static/media/geometryBB.3917d0a7.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "802b6a7e831d785b9970",
+    "url": "/static/js/main.4db0d1f4.chunk.js"
+  },
+  {
+    "revision": "5c2d77d474db2df5b4da",
+    "url": "/static/js/2.69a8b221.chunk.js"
+  },
+  {
+    "revision": "802b6a7e831d785b9970",
+    "url": "/static/css/main.681401fd.chunk.css"
+  },
+  {
+    "revision": "43d87a402ccee3a2aa7b5bb2def2df7d",
+    "url": "/index.html"
+  }
+];
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -5,11 +5,17 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fptesting");
 
 const householdSeed =
 {
-    name: "Demo Household"
+    _id: "demohh01",
+    name: "Demo Household",
+    users: ["demous01", "demous02"],
+    expenses: ["demoex01", "demoex02"],
+    finances: ["demofi01", "demofi02"],
+    schedules: ["demosc01", "demosc02"]
 }
 
 const expensesSeed = [
     {
+        _id: "demoex01",
         name: "Electric Bill",
         description: "Monthly electric bill",
         amount: 63,
@@ -19,6 +25,7 @@ const expensesSeed = [
         }
     },
     {
+        _id: "demoex02",
         name: "Netflix",
         description: "Monthly netflix fee",
         amount: 10,
@@ -30,32 +37,116 @@ const expensesSeed = [
 ]
 
 const financeSeeds = [
-    {name: "Paycheck",
-    description: "Biweekly paycheck",
-    amount: 650,
-    calendar: {
-        title: "Pay Day",
-        start: "2019-04-12"
-    }},
-    {name: "Paycheck",
-    description: "Biweekly paycheck",
-    amount: 650,
-    calendar: {
-        title: "Pay Day",
-        start: "2019-04-26"
-    }}
+    {
+        _id: "demofi01",
+        name: "Paycheck",
+        description: "Biweekly paycheck",
+        amount: 650,
+        calendar: {
+            title: "Pay Day",
+            start: "2019-04-12"
+        }
+    },
+    {
+        _id:"demofi02",
+        name: "Paycheck",
+        description: "Biweekly paycheck",
+        amount: 650,
+        calendar: {
+            title: "Pay Day",
+            start: "2019-04-26"
+        }
+    }
 ]
 
 const userSeeds = [
     {
+        _id: "demous01",
         username: "Admin",
         email: "Admin@example.com",
-        password: "Admin"
+        password: "Admin",
+        hhID: "demohh01"
     },
     {
+        _id: "demous02",
         username: "Demo",
         email: "Demo@example.com",
-        password: "Demo"
+        password: "Demo",
+        hhID: "demohh01"
     }
 ]
 
+const scheduleSeeds = [
+    {
+        _id: "demosc01",
+        calendar: {
+            title: "Soccer Game",
+            start: "2019-04-01T13:30:00",
+            allDay: false
+        }
+    },
+    {
+        _id: "demosc",
+        calendar: {
+            title: "Vacation",
+            start: "2019-04-03",
+            end: "2019-04-08"
+        }
+    }
+]
+
+
+function runSeeds() {
+    db.Household
+        .remove({})
+        .then(() => db.Household.collection.insert(householdSeed))
+        .then(data => {
+            console.log(data. result.n + " households inserted");
+        })
+        .catch(err => {
+            console.error(err)
+        });
+
+    db.Expense
+        .remove({})
+        .then(() => db.Expense.collection.insert(expensesSeed))
+        .then(data => {
+            console.log(data. result.n + " expenses inserted");
+        })
+        .catch(err => {
+            console.error(err)
+        });
+
+    db.Finance
+        .remove({})
+        .then(() => db.Finance.collection.insert(financeSeeds))
+        .then(data => {
+            console.log(data. result.n + " finances inserted");
+        })
+        .catch(err => {
+            console.error(err)
+        });
+    db.User
+        .remove({})
+        .then(() => db.User.collection.insert(userSeeds))
+        .then(data => {
+            console.log(data. result.n + " users inserted");
+        })
+        .catch(err => {
+            console.error(err)
+        });
+    db.Schedule
+        .remove({})
+        .then(() => db.Schedule.collection.insert(scheduleSeeds))
+        .then(data => {
+            console.log(data. result.n + " schedules inserted");
+        })
+        .catch(err => {
+            console.error(err)
+        });   
+
+
+
+}
+
+runSeeds();
