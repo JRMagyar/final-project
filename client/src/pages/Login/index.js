3b7f8020e51b9d5977c20e468d03f015d78de3e4
Mtnman <billman.designs@gmail.com>
2019-04-13 12:16:58 -0400

    i made more changes, julia let's merge this too

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { FlexCol } from "../../components/FlexWrap/index";
 import "./style.css"
 import axios from "axios";
+import MediaQuery from 'react-responsive';
 
 
 class Login extends React.Component {
@@ -26,27 +27,55 @@ class Login extends React.Component {
     render(){
 
         return (
-            <div className="hero-bkg-animated">
-                <FlexCol>
-                    <article className="login-area">
-                        <div className="title">
-                            <h1>
-                                welcome to uncle
-                            </h1>
-                            <p>
-                                a friendly (but not to friendly) household management app
-                            </p>
+            <MediaQuery minDeviceWidth={700}>
+                {(matches) => {
+                    if (matches) {
+                        return <div className="hero-bkg-animated">
+                            <FlexCol>
+                                <article className="login-area">
+                                    <div className="title">
+                                        <h1>
+                                            welcome to uncle
+                                        </h1>
+                                        <p>
+                                            a friendly (but not to friendly) household management app
+                                        </p>
+                                    </div>
+                                    <form className="signin-form">
+                                        <input type="text" name="username" placeholder="enter username"/>
+                                        <input type="password" name="password" autoComplete="on" placeholder="enter password"/>
+                                        <input type="submit" onClick={this.login} value="Login" />
+                                    </form>
+                                    {/* <p>or</p>
+                                    <article class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></article> */}
+                                </article>
+                            </FlexCol>
                         </div>
-                        <form className="signin-form">
-                            <input type="text" name="username" placeholder="enter username"/>
-                            <input type="password" name="password" autoComplete="on" placeholder="enter password"/>
-                            <input type="submit" onClick={this.login} value="Login" />
-                        </form>
-                        {/* <p>or</p>
-                        <article class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></article> */}
-                    </article>
-                </FlexCol>
-            </div>
+                    } else {
+                        return <div className="hero-bkg-animated">
+                            <FlexCol>
+                                <article className="login-areaM">
+                                    <div className="title">
+                                        <h1>
+                                            welcome to uncle
+                                        </h1>
+                                        <p>
+                                            a friendly (but not to friendly) household management app
+                                        </p>
+                                    </div>
+                                    <form className="signin-form">
+                                        <input type="text" name="username" placeholder="enter username"/>
+                                        <input type="password" name="password" autoComplete="on" placeholder="enter password"/>
+                                        <input type="submit" onClick={this.login} value="Login" />
+                                    </form>
+                                    {/* <p>or</p>
+                                    <article class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></article> */}
+                                </article>
+                            </FlexCol>
+                        </div>
+                    }
+                }}
+            </MediaQuery>
         );
     }
 }
